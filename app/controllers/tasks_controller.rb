a008793d87f1710cd2960ec0d0aaf99eed362213
Thibaud Gerin <thibaud.gerin@gmail.com>
2020-05-26 12:37:01 +0200

    Create a task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,4 +12,21 @@ class TasksController < ApplicationController
     @task = Task.new
   end
   
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      flash[:success] = "Task successfully created"
+      redirect_to @task
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
+    end
+  end
+
+  private 
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+
 end
